748a3fd8d7de5ec5cfff4ab097de6c401fce4232
SOFTSOLUTION\Md Azim Mina <mdazimmina@gmail.com>
2023-10-20 10:32:48 +0600

    Add project files.

---
+++ BookShoop_App/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BookShoop_App.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookShoop_App.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ BookShoop_App/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace BookShoop_App.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Status { get; set; }
+        
+    }
+}
